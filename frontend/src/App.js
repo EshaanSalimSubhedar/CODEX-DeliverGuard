2ea10da2cca1b98bac0d0fc4138ebd520026d445
EshaanSalimSubhedar <subhedareshaan@gmail.com>
2026-04-05 22:56:26 +0530

    redo frontend connection

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,7 +4,7 @@ import React, { useState } from "react";
 import { MapContainer, TileLayer, Marker } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
 
-const BASE_URL = "https://deliverguard-backend-43e3.onrender.com/";
+const BASE_URL = "https://deliverguard-backend-43e3.onrender.com";
 
 function App() {
     const [screen,setScreen] = useState("home");
